dba08ef411e551f31317fca4bf26127daf723156
itsdjodjo <tsoaramaherison7@gmail.com>
2022-07-13 12:10:41 +0300

    add home popup

---
+++ scripts/home.js
@@ -0,0 +1,10 @@
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const popupContainer = document.getElementById('popupContainer');
+
+open.addEventListener('click', () => {
+    popupContainer.classList.add('active');
+});
+close.addEventListener('click', () => {
+    popupContainer.classList.remove('active');
+});
