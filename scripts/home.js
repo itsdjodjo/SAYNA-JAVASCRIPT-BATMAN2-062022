748d0e40fa1adc80263d0d12c7d031af7e27ac35
itsdjodjo <tsoaramaherison7@gmail.com>
2022-08-02 13:21:52 +0300

    ajout de l'animation fadeIn vers la gauche

--- scripts/home.js
+++ scripts/home.js
@@ -9,3 +9,24 @@ open.addEventListener('click', () => {
 close.addEventListener('click', () => {
     popupContainer.classList.remove('active');
 });
+
+
+// fade in
+window.addEventListener('scroll', reveal)
+
+function reveal() {
+    var faders = document.querySelectorAll('.fade-in');
+
+    for(let i = 0; i < faders.length; i++) {
+        let windowHeight = window.innerHeight;
+        let revealTop = faders[i].getBoundingClientRect().top;
+        let revealPoint = 150;
+
+        if(revealTop < windowHeight - revealPoint) {
+            faders[i].classList.add('show-content');
+        }
+        else {
+            faders[i].classList.remove('show-content');
+        }
+    }
+};
